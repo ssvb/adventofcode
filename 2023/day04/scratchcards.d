3641c1f37c50dcc5d94fe3b0f9fe9ef0a79c09f6
Siarhei Siamashka <siarhei.siamashka@gmail.com>
2023-12-04 09:11:28 +0200

    The 4th day of Advent of Code 2023

---
+++ 2023/day04/scratchcards.d
@@ -0,0 +1,16 @@
+import std;
+void main() {
+  auto cards = stdin.byLine.map!(s =>
+    s.split(':')[1].split('|').map!(x => x.split.map!(to!int).array));
+
+  auto wins = cards.map!(card =>
+    setIntersection(card[0].sort, card[1].sort).array.length).array;
+
+  wins.map!(cnt => (2L ^^ cnt) / 2).sum.writefln!"Part1: %d";
+
+  auto cardcopies = new long[](wins.length);
+  cardcopies[] = 1;
+  foreach (i, cnt ; wins)
+    cardcopies[i + 1 .. min(i + 1 + cnt, wins.length)] += cardcopies[i];
+  cardcopies.sum.writefln!"Part2: %d";
+}
