2d8827b58b3aff2ceb59cfb9e907bb2d3dfd4585
Siarhei Siamashka <siarhei.siamashka@gmail.com>
2023-12-07 18:55:59 +0200

    2023 day 07: Rename 'Deck' to 'Hand' and other cosmetic changes

--- 2023/day07/camel_cards.d
+++ 2023/day07/camel_cards.d
@@ -1,8 +1,8 @@
 import std;
 
-struct Deck { int[] cards; long bid; }
+struct Hand { int[] cards; long bid; }
 
-auto eval_deck_type(int[] cards) {
+auto eval_hand_type(int[] cards) {
   auto freq = new int[](max(cards.maxElement + 1, 5));
   int jokers = 0;
   foreach (card ; cards) {
@@ -24,29 +24,26 @@ auto eval_deck_type(int[] cards) {
     return 3; // two pair
   if (freq[0] + jokers == 2)
     return 2; // one pair
-  if (freq[4] == 1)
-    return 1; // high card
-  return 0;
+  return 1;   // high card
 }
 
-long solve(string[] input_lines, int[dchar] card_strength)
-{
-  auto decks = input_lines.map!(s => s.split)
-    .map!(x => Deck(x[0].map!(card => card_strength[card]).array,
+long solve(string[] input_lines, int[dchar] card_strength) {
+  auto hands = input_lines.map!(s => s.split)
+    .map!(x => Hand(x[0].map!(card => card_strength[card]).array,
                     x[1].to!long)).array;
 
-  bool decks_comparator(ref Deck deck1, ref Deck deck2) {
-    int deck1_type = eval_deck_type(deck1.cards);
-    int deck2_type = eval_deck_type(deck2.cards);
-    if (deck1_type == deck2_type)
-      return deck1.cards < deck2.cards;
-    return deck1_type < deck2_type;
+  bool hands_comparator(ref Hand hand1, ref Hand hand2) {
+    int hand1_type = eval_hand_type(hand1.cards);
+    int hand2_type = eval_hand_type(hand2.cards);
+    if (hand1_type == hand2_type)
+      return hand1.cards < hand2.cards;
+    return hand1_type < hand2_type;
   }
-  decks.sort!decks_comparator;
+  hands.sort!hands_comparator;
 
   long answer = 0;
-  foreach (i, deck ; decks)
-    answer += (i + 1) * deck.bid;
+  foreach (rank, hand ; hands)
+    answer += (rank + 1) * hand.bid;
   return answer;
 }
 
@@ -54,8 +51,8 @@ void main() {
   auto input_lines = stdin.byLineCopy.array;
 
   int[dchar] card_strength;
-  foreach (i, card ; "A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2".split(", ").retro.array)
-    card_strength[card[0]] = i.to!int;
+  foreach (idx, card_label ; "AKQJT98765432".retro.array)
+    card_strength[card_label] = idx.to!int;
 
   input_lines.solve(card_strength).writefln!"Part1: %d";
 
